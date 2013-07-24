7fa1df22399705d3efd13b26d40dc5911c0eee9c
Tak Tran <contact@tutaktran.com>
2013-07-24 09:19:28 +0100

    Add support for Firebase.

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,14 @@
 (function (){
   'use strict';
 
+  $(document).ready(function () {
+    var dataRef = new Firebase('https://darkness.firebaseIO.com/');
+
+    dataRef.child("x" + ":" + "y").set("something");
+
+    dataRef.on('value', function(data) {
+      console.log(data.val());
+    });
+  });
 
 })();
