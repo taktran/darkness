6bfbffbbb2dd4578b39b18aa18e5be4984e3ca61
Tak Tran <contact@tutaktran.com>
2013-07-24 10:58:00 +0100

    Got drawing and redrawing working.

--- app/js/main.js
+++ app/js/main.js
@@ -21,29 +21,19 @@
       var dataVal = data.val();
 
       if (dataVal && _.has(dataVal, "lines")) {
-        var linesData = dataVal.lines,
-          lines = _.values(linesData),
-          linesArray = [];
-
-        _.each(lines, function(line) {
-          var lineHash = JSON.parse(line)[0];
-          linesArray.push(lineHash);
-        });
-        console.log(linesArray.length +  " lines");
-
-        sketchpad.strokes(linesArray);
+        var strokes = dataVal.lines;
+        sketchpad.json(strokes);
       }
 
-      dataRef.child("lines").on('child_added', function(data) {
+      dataRef.child("lines").on('value', function(data) {
         var lines = data.val();
-        // console.log("child_added", lines);
-        // sketchpad.json(lines);
+        sketchpad.json(lines);
       });
 
       // When the sketchpad changes, upload data
       sketchpad.change(function() {
         var data = sketchpad.json();
-        dataRef.child("lines").push(data);
+        dataRef.child("lines").set(data);
       });
     });
   });
