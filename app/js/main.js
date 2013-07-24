09fff5788249f8491f15424f4dee9692a97f75e6
Tak Tran <contact@tutaktran.com>
2013-07-24 09:59:28 +0100

    Add sketchpad to draw and record commands.

--- app/js/main.js
+++ app/js/main.js
@@ -1,14 +1,36 @@
 (function (){
   'use strict';
+  var dataRef = new Firebase('https://darkness.firebaseIO.com/');;
 
   $(document).ready(function () {
-    var dataRef = new Firebase('https://darkness.firebaseIO.com/');
-
-    dataRef.child("x" + ":" + "y").set("something");
-
     dataRef.on('value', function(data) {
-      console.log(data.val());
+      console.log("value", data.val());
     });
+
+    dataRef.child("lines").on('child_added', function(data) {
+      console.log("child_added", data.val());
+    });
+
+    var sketchpad = Raphael.sketchpad("editor", {
+      width: 900,
+      height: 800,
+      editing: true
+    });
+
+    var pen = sketchpad.pen();
+    pen.color("#0000ff");
+    pen.opacity(0.5);
+
+    // When the sketchpad changes, update the input field.
+    sketchpad.change(function() {
+      var data = sketchpad.json();
+      dataRef.child("lines").push(data);
+    });
+
+    // Global variables (for debugging)
+    window.App = {
+      sketchpad: sketchpad
+    };
   });
 
 })();
